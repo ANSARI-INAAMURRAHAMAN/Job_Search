a3c5a933596bf1474b0b40004fd738d4c1a4f5ea
ANSARI-INAAMURRAHAMAN <me220003009@iiti.ac.in>
2025-06-06 18:26:48 +0530

    DONE

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,3 +1,5 @@
-const API_BASE_URL = 'https://job-search-ypji.onrender.com/api/v1';
+const API_BASE_URL = process.env.NODE_ENV === 'production' 
+  ? "https://job-search-ypji.onrender.com/api/v1"
+  : "http://localhost:4000/api/v1";
 
 export default API_BASE_URL;
